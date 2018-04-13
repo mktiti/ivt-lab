7ae76001b922b7ef59e6cfec266084dc5c2e5a48
Márkus Krisztián <markuskrisztian1@gmail.com>
2018-04-12 23:59:25 -0700

    torpedo, laser TODOs fixed

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,8 +16,7 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
-    return false;
+    return true;
   }
 
   /**
@@ -77,9 +76,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
-
+	firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
         break;
     }
 
