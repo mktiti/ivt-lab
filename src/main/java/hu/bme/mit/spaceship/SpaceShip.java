439c045c48ade6731860a55c9d35d95e2784e94a
Márkus Krisztián <markuskrisztian1@gmail.com>
2018-04-13 00:06:15 -0700

    Torpedo fire javadoc updated

--- src/main/java/hu/bme/mit/spaceship/SpaceShip.java
+++ src/main/java/hu/bme/mit/spaceship/SpaceShip.java
@@ -15,7 +15,7 @@ public interface SpaceShip {
   public boolean fireLaser(FiringMode firingMode);
 
   /**
-  * Fires the torpedo stores of the ship
+  * Fires the torpedo stores of the ship with the specified firing mode
   *
   * @param firingMode how many torpedo stores to fire
   * @return whether the fire command was successful
